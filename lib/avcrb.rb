2011b5cf3c5b06a962cccdc95035b7ce04006ca9
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-01-30 15:54:12 -0800

    not do dumb thing with autoload

--- lib/avcrb.rb
+++ lib/avcrb.rb
@@ -2,5 +2,5 @@ require "avcrb/version"
 
 
 module Avcrb
-  autoload :FrameRipper, 'bundler/frame_ripper'  # Your code goes here...
+  autoload :FrameRipper, 'avcrb/frame_ripper'  # Your code goes here...
 end
